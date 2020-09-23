a1d411ce710d8fbcbcebe2cbd73959c39d00f2ad
Arijit Das <arijit@chainsafe.io>
2020-09-23 19:14:26 +0530

    Modify scripts.

--- run_lotus.sh
+++ run_lotus.sh
@@ -4,6 +4,7 @@ set -o xtrace
 rm -rf ~/.lotus ~/.genesis-sectors
 export LOTUS_SKIP_GENESIS_CHECK=_yes_
 LOTUS_DIR=./extern/lotus
+$LOTUS_DIR/lotus fetch-params 2048
 $LOTUS_DIR/lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
 $LOTUS_DIR/lotus-seed genesis new localnet.json
 $LOTUS_DIR/lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
--- run_market.sh
+++ run_market.sh
@@ -2,43 +2,6 @@
 
 set -o xtrace
 
-NODE_PORT=1234
-MINER_PORT=2345
-MARKET_PORT=8888
-
-LOTUS_NODE_ADDR=127.0.0.1:$NODE_PORT/rpc/v0
-while true
-do
-    http_code=$(curl --output /dev/null -w ''%{http_code}'' --fail $LOTUS_NODE_ADDR --header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0", "method": "Filecoin.Version", "params": [], "id": 1}')
-    if [ "$http_code" -eq 200 ]; then
-		  echo "Lotus node is up"
-		  break
-	  fi
-    printf 'Waiting for lotus daemon initialization'
-    sleep 5
-done
-
-LOTUS_MINER_ADDR=127.0.0.1:$MINER_PORT/rpc/v0
-while true
-do
-    http_code=$(curl --output /dev/null -w ''%{http_code}'' --fail $LOTUS_MINER_ADDR --header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0", "method": "Filecoin.Version", "params": [], "id": 1}')
-    if [ "$http_code" -eq 200 ]; then
-		  echo "Lotus miner is up"
-		  break
-	  fi
-    printf 'Waiting for lotus miner initialization'
-    sleep 5
-done
-
-LOTUS_BIN=./extern/lotus/lotus
-NODE_TOKEN=$($LOTUS_BIN auth create-token --perm admin)
-
-LOTUS_MINER_BIN=./extern/lotus/lotus-miner
-MINER_TOKEN=$($LOTUS_MINER_BIN auth create-token --perm admin)
-
-export NODE_API_INFO=$NODE_TOKEN:/ip4/127.0.0.1/tcp/$NODE_PORT/ws
-export MARKET_API_INFO=$NODE_TOKEN:/ip4/127.0.0.1/tcp/$MARKET_PORT/ws
-export MINER_API_INFO=$MINER_TOKEN:/ip4/127.0.0.1/tcp/$MINER_PORT/ws
-
+source ./init_market_env.sh
 go build -gcflags='-N -l' -tags=2k
 ./go-fil-markets-interface
