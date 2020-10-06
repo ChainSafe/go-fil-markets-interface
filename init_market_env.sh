b0682094b2278504964f544e4f7a17b772653630
Arijit Das <arijit@chainsafe.io>
2020-10-06 10:57:37 +0530

    Run docker container and E2E test from a script.

--- init_market_env.sh
+++ init_market_env.sh
@@ -7,36 +7,39 @@ MARKET_PORT=8888
 ENV_IP=${ENV_IP:=0.0.0.0}
 
 LOTUS_NODE_ADDR=$ENV_IP:$NODE_PORT/rpc/v0
-while true
-do
+while true; do
     http_code=$(curl --output /dev/null -w ''%{http_code}'' --fail $LOTUS_NODE_ADDR --header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0", "method": "Filecoin.Version", "params": [], "id": 1}')
     if [ "$http_code" -eq 200 ]; then
-		  echo "Lotus node is up"
-		  break
-	  fi
+        echo "Lotus node is up"
+        break
+    fi
     printf 'Waiting for lotus daemon initialization'
     sleep 5
 done
 
 LOTUS_MINER_ADDR=$ENV_IP:$MINER_PORT/rpc/v0
-while true
-do
+while true; do
     http_code=$(curl --output /dev/null -w ''%{http_code}'' --fail $LOTUS_MINER_ADDR --header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0", "method": "Filecoin.Version", "params": [], "id": 1}')
     if [ "$http_code" -eq 200 ]; then
-		  echo "Lotus miner is up"
-		  break
-	  fi
+        echo "Lotus miner is up"
+        break
+    fi
     printf 'Waiting for lotus miner initialization'
     sleep 5
 done
 
 LOTUS_DIR=${LOTUS_DIR:=./extern/lotus}
-
 LOTUS_BIN=$LOTUS_DIR/lotus
-NODE_TOKEN=$($LOTUS_BIN auth create-token --perm admin)
+CONTAINER_NAME=${CONTAINER_NAME:=go-fil-markets-e2e}
+
+NODE_TOKEN=""
+if [ -z "$DOCKER" ]; then
+    NODE_TOKEN=$($LOTUS_BIN auth create-token --perm admin)
+else
+    NODE_TOKEN=$(docker exec -it $CONTAINER_NAME /app/lotus/lotus auth create-token --perm admin | tr -d '\r')
+fi
 
 if [[ "$NODE_TOKEN" =~ "ERROR" ]]; then
-    echo "$NODE_TOKEN"
     exit 1
 fi
 
@@ -49,7 +52,13 @@ echo "MARKET_API_INFO=$MARKET_API_INFO"
 export MARKET_API_INFO=$MARKET_API_INFO
 
 LOTUS_MINER_BIN=$LOTUS_DIR/lotus-miner
-MINER_TOKEN=$($LOTUS_MINER_BIN auth create-token --perm admin)
+
+MINER_TOKEN=""
+if [ -z "$DOCKER" ]; then
+    MINER_TOKEN=$($LOTUS_MINER_BIN auth create-token --perm admin)
+else
+    MINER_TOKEN=$(docker exec -it $CONTAINER_NAME /app/lotus/lotus-miner auth create-token --perm admin | tr -d '\r')
+fi
 
 if [[ "$MINER_TOKEN" =~ "ERROR" ]]; then
     echo "$MINER_TOKEN"
--- run_docker.sh
+++ run_docker.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 
-make submodule
+CONTAINER_NAME=go-fil-markets-e2e
+function cleanup {
+    # Stop the docker container
+    docker stop $CONTAINER_NAME
+    docker container rm $CONTAINER_NAME
+}
+trap cleanup EXIT
+cleanup
 
-cp /app/go-fil-markets/config/lotus/config.toml ~/lotus/config.toml
-cp /app/go-fil-markets/config/lotusminer/config.toml ~/lotusminer/config.toml
+# Pull and run the docker image
+docker pull arijitad/go-fil-markets:latest
+docker run -d --name $CONTAINER_NAME -p 1234:1234 -p 2345:2345 -p 8888:8888 --rm -it go-fil-markets
 
-# Start lotus node
-LOTUS_DIR=/app/lotus ./run_lotus.sh &
+# Initialize the env variables
+DOCKER=_yes_ CONTAINER_NAME=$CONTAINER_NAME source ./init_market_env.sh
 
-# Start lotus miner
-LOTUS_DIR=/app/lotus ./run_lotus_miner.sh &
+# Wait for market initialization to complete
+ENV_IP=${ENV_IP:=0.0.0.0}
+MARKET_PORT=8888
+MARKET_ADDR=$ENV_IP:$MARKET_PORT/rpc/v0
+while true; do
+    http_code=$(curl --output /dev/null -w ''%{http_code}'' --fail $MARKET_ADDR --header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0", "method": "Market.ClientListDataTransfers", "params": [], "id": 1}')
+    if [ "$http_code" -eq 200 ]; then
+        echo "Market is up"
+        break
+    fi
+    printf 'Waiting for Market daemon initialization'
+    sleep 5
+done
 
-# Start market
-./run_market.sh
+# Run the test
+cd cmd/client
+go test -v -timeout 60m -run ^TestMarketStorage$
+go test -v -timeout 60m -run ^TestMarketRetrieval$
+
+cleanup
